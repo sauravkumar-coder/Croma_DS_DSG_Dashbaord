3c87a769e962e3272737ca210a7352d3aab4a158
Yoganshu Sharma <yoganshusharma@bitbucket.org>
2026-06-24 22:47:41 +0530

    chore: update database connection details to new auth-enabled docker URI

--- salesdost_samsung/backend/database.py
+++ salesdost_samsung/backend/database.py
@@ -4,7 +4,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 logger = logging.getLogger(__name__)
 
-MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://127.0.0.1:27018/zoppertrack?directConnection=true")
+MONGO_DETAILS = os.getenv("MONGO_URI", "mongodb://saurav:saurav12@172.17.0.1:27017/zoppertrack?authSource=zoppertrack&directConnection=true")
 
 client: AsyncIOMotorClient | None = None
 database = None
